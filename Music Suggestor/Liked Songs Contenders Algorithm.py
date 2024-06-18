9ebd9466620addefa5b0b2ef372d09c8cfa8f1d1
pafari <papa-yaw_afari@brown.edu>
2024-06-18 19:24:49 -0400

    test

---
+++ Music Suggestor/Liked Songs Contenders Algorithm.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+
+'''Step 1: Data Preparation and EDA: I am excited to be working on something
+that is actually useful for my everyday life. Since I spent too much time looking at my google document trying to know which songs to add to my playlist
+it is really rewarding to see that will be definitely made significantly easier starting today'''
+
